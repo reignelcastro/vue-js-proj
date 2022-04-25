2bdae2419c401089b7b93bd12fd391114e4e3e2d
reignelcastro <www.reignelcastro>
2022-04-25 16:27:32 +0800

    kick starter

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,21 +4,22 @@
 <template>
     
         <div class="d-flex align-items-center justify-content-center flex-column vh-100"> 
-            <h2>Login Page</h2>
-            <div class="unm w-50">
-                <label for="username">Username: </label>
-                <input type="text" class="form-control">
-            </div>
-            <div class="pss w-50">
-                <label for="username">Password: </label>
-                <input type="text" class="form-control">
-            </div>
-            <div class="btns mt-2 w-50">
-                <button class="btn m-1 btn-primary">Login</button>
-                <button class="btn m-1 btn-info">Sign up</button>
+            <div class="w-40">
+                <h2>Login Page</h2>
+                <div class="unm">
+                    <label for="username">Username: </label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="pss">
+                    <label for="password">Password: </label>
+                    <input type="password" class="form-control">
+                </div>
+                <div class="btns mt-2">
+                    <button class="btn m-1 btn-primary">Login</button>
+                    <button class="btn m-1 btn-info"><RouterLink to="signup">Sign up</RouterLink></button>
+                </div>
             </div>
         </div>
-    
-</template>
+</template> 
 <style scoped>
 </style>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+<template>
+    <div class="d-flex align-items-center justify-content-center flex-column vh-100" id="form-group">
+        <h2>Sign Up</h2>
+        <div class="form-group w-25">
+                <div class="fn">
+                    <label for="name">Fullname: </label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="sid">
+                    <label for="id">School ID: </label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="unm">
+                    <label for="username">Username: </label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="pss">
+                    <label for="password">Password: </label>
+                    <input type="password" class="form-control">
+                </div>
+                <div class="pss">
+                    
+                    <button class="btn m-1 btn-info">Save</button>
+                    <button class="btn m-1 btn-danger">Cancel</button>
+                </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+
+#form-group{
+        background-color: aqua;
+
+};
+</style>
